d988c8a68261761599276acf967dc8b7611d7741
andrewstagnaro <42977270+andrewstagnaro@users.noreply.github.com>
2018-11-01 13:37:50 -0400

    Add files via upload

---
+++ CreditCard/src/CreditCard.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ * @author Andrew Stagnaro
+ * creates a credit card
+ * Pd.6
+ *
+ */
+public class CreditCard 
+{
+	//Fields
+	private final int CARDNUM;
+	private double limit;
+	private double balance;
+	private double intrestRate;
+	private String holderName;
+	
+	//Constructors
+	/**
+	 * 
+	 * @param cN sets CARDNUM = cN
+	 * @param lmt sets limit = lmt
+	 * @param intRt sets intrestRate = intRt
+	 * @param hldrNm sets holderName = hldrNm
+	 */
+	public CreditCard(int cN, double lmt, double intRt, String hldrNm)
+	{
+		CARDNUM = cN;
+		lmt = limit;
+		intRt = intrestRate;
+		hldrNm = holderName;
+		balance = 0;
+	}
+	
+	//Methods
+	
+	public void charge(double i)
+	{
+		if(i < 5000)
+		{
+			i++;
+		}
+	}
+
+
+
+
+
+}
---
+++ CreditCard/src/Main.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ * @author Andrew Stagnaro
+ *
+ */
+public class Main 
+{
+
+	public static void main(String[] args) 
+	{
+		CreditCard[] cards = new CreditCard[5];
+		cards [0] = CreditCard(1234567890, 10000, 0, 0.14, Tony Smith)
+		
+	}
+
+}
